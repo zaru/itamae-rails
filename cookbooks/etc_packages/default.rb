7b7e1a72e8e10f18d27eb6a3f943379b5a3bac53
zaru <sakuraba@basicinc.jp>
2016-01-28 16:22:24 +0900

    add packages

--- cookbooks/etc_packages/default.rb
+++ cookbooks/etc_packages/default.rb
@@ -1,3 +1,23 @@
-%w( git ImageMagick ImageMagick-devel gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel libxslt-devel openssl-devel make bzip2 autoconf automake libtool bison).each do |pkg|
+%w( git
+sqlite-devel
+ImageMagick
+ImageMagick-devel
+gcc-c++
+patch
+readline
+readline-devel
+zlib
+zlib-devel
+libyaml-devel
+libffi-devel
+libxslt-devel
+openssl-devel
+make
+bzip2
+autoconf
+automake
+libtool
+bison
+nodejs ).each do |pkg|
   package pkg
 end
